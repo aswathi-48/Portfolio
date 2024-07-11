9d10658f46dcc4d1f02ab7f4a8c766a3035a05d1
aswathi-48 <aswathik482@gmail.com>
2024-07-11 21:50:12 +0530

    my portfolio

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const NavItems = styled.ul`
     list-style: none;
 `
 const NavLink = styled.a`
-    background-color: ${({ theme }) => theme.text_primary};
+    color: ${({ theme }) => theme.text_primary};
     font-weight: 500;
     cursor: pointer;
     transition: all 0.2s ease-in-out;
@@ -40,6 +40,36 @@ const NavLink = styled.a`
     }
 `
 
+const ButtonContainer = styled.div`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  padding: 0 6px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const GithubButton = styled.a`
+  border: 1px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primary};
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+  cursor: pointer;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: 500;
+  transition: all 0.6s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    background: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
 const Navbar = () => {
   return (
     <NavbarContainer>
@@ -52,6 +82,15 @@ const Navbar = () => {
             <NavLink href="#Education">Education </NavLink>
 
         </NavItems>
+
+          {/* <GithubButton href={Bio.github} target="_Blank"> */}
+        {/* <ButtonContainer>
+          <GithubButton >
+
+            Github Profile
+          </GithubButton>
+        </ButtonContainer> */}
+
     </NavbarContainer>
   )
 }
