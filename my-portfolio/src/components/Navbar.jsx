9245e0fe69573a02e61bf9478e8b83403dadbc70
aswathi-48 <aswathik482@gmail.com>
2024-07-26 23:11:29 +0530

    contact added

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -132,7 +132,7 @@ const Navbar = () => {
   return (
     <Nav>
       <NavbarContainer>
-        <NavLogo to="/">Logo</NavLogo>
+        <NavLogo to="/"></NavLogo>
 
         <MobileIcon onClick={() => setIsOpen(!isOpen)}>
           <MenuRounded style={{ color: "inherit" }} />
--- my-portfolio/src/data/constants.js
+++ my-portfolio/src/data/constants.js
@@ -7,8 +7,8 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/aswathi-48",
-  // resume:
-  //   "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
+  resume:
+  "https://drive.google.com/file/d/1N0017ZpwsQdKFTBXiVYc2y-pvWYmWzep/view?usp=drivesdk",
     linkedin: "www.linkedin.com/in/aswathi-k312",
   // twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/__dream__girl___12/",
