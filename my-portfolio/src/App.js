02b41f61206bf51e6cb0a47c7164729902fe3f24
aswathi-48 <aswathik482@gmail.com>
2024-07-26 23:02:04 +0530

    wrapper

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -15,6 +15,23 @@ const Body = styled.div`
   overflow-x: hidden;
   position: relative;
 `;
+
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 function App() {
   return (
 
@@ -23,11 +40,16 @@ function App() {
     <Navbar/>
       <Body>
         <Hero/>
+        <Wrapper>
         <Skills/>
         <Experience/>
-        <Education/>
+        </Wrapper>
         <Projects/>
+        <Wrapper>
+        <Education/>
         <Contact/>
+        </Wrapper>
+
       </Body>
     </BrowserRouter>
     </ThemeProvider>
